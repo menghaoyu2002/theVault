9ea04cff88a4e70b844bb546ad3b8ef638ad80ee
Menghao Yu <menghaoyu2002@gmail.com>
2022-01-30 18:32:45 -0500

    created the images folder if it doesn't exist. fixed bug where multer was trying to save to a folder that doesn't exist

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,10 +1,13 @@
+import * as fs from 'fs';
 import multer from 'multer';
 import path from 'path';
 
 // set up multer
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'src/public/images');
+        const pathName = path.join(__dirname, '../public/images');
+        fs.mkdirSync(pathName, { recursive: true }); // if the folder doesn't exist, create it
+        cb(null, pathName);
     },
     filename: function (req, file, cb) {
         cb(null, Date.now().toString() + path.extname(file.originalname));
