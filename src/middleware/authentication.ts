d71ca408fc6dc2a38079f395df648db59e716c46
Menghao Yu <menghaoyu2002@gmail.com>
2022-02-03 01:05:48 -0500

    used cookies instead of sending the token and requiring an auth header

--- src/middleware/authentication.ts
+++ src/middleware/authentication.ts
@@ -8,9 +8,7 @@ export async function authenticateToken(
     next: NextFunction
 ) {
     try {
-        const token =
-            req.headers.authorization &&
-            req.headers.authorization.split(' ')[1];
+        const token = req.cookies.access_token;
 
         if (!token) {
             return res.sendStatus(401);
